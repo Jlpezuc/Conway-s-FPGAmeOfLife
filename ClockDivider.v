8a55bb2d600e7dee6f54c4e3ee588c3a2033f3ff
Jlpezuc <69552420+Jlpezuc@users.noreply.github.com>
2024-08-02 01:30:38 -0400

    Update ClockDivider.v

--- ClockDivider.v
+++ ClockDivider.v
@@ -1,37 +1,18 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 08.06.2024 20:37:01
-// Design Name: 
-// Module Name: ClockDivider
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
 
-
-module ClockDivider#(parameter divisor = 2)
+module ClockDivider
     (
     input reset,
     input clock_in,
+    input divisor,
     output clock_out
     );
     
-    reg [$clog2(divisor + 1) - 1:0] count = 0;
+    reg [31:0] count = 0;
     
     assign clock_out = (count > divisor / 2) ? 1 : 0;
     
-    always @(posedge clock_in or negedge reset) begin
+    always @(posedge clock_in) begin
         if (!reset) begin
             count <= 0;
         end else begin
